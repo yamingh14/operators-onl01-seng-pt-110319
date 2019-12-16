c27484caf52e65199f38ed5af10e19ff45d368a5
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-16 14:44:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -16,7 +16,7 @@ end
 def not_safe?(speed)
 <<<<<<< HEAD
 speed > 60 || speed < 40 ? true : false
-=======
+# =======
 
->>>>>>> 25ce8aae17b51fde68cbc9519e618341e870ab67
+# >>>>>>> 25ce8aae17b51fde68cbc9519e618341e870ab67
 end
