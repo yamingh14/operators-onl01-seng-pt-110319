87d615bcb7ea150164bda9da2adef12f7597bb8a
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-13 01:45:36 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,35 @@
 def unsafe?(speed)
-	if speed > 60 || speed < 40
-		return true
-			else
-		return false
-	end
+	speed < 40 || speed > 60 ? true : false
 end
 
 def not_safe?(speed)
-speed > 60 || speed < 40 ? true : false
+	speed > 60 || speed < 40 ? true : false
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def unsafe?(speed)
+# 	if speed > 60 || speed < 40
+# 		return true
+# 			else
+# 		return false
+# 	end
+# end
+#
+# def not_safe?(speed)
+# speed > 60 || speed < 40 ? true : false
+# end
